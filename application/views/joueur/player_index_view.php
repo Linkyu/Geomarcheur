c2bcf04ae32fa8748a684e6d5c45f8f0dd4e4696
Linkyu <linkyu@users.noreply.github.com>
2017-08-05 02:53:07 +0200

    Initial feature commit #7

---
+++ application/views/joueur/player_index_view.php
@@ -0,0 +1,15 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title></title>
+</head>
+<body>
+
+
+
+</body>
+</html>
