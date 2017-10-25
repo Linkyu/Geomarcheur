18ddbb8fc3ff925d73029ff5e27de9dfad32ec7d
Linkyu <linkyu@users.noreply.github.com>
2017-10-25 16:23:39 +0200

    Create login interface #52

--- application/views/login.php
+++ application/views/login.php
@@ -8,6 +8,8 @@ $temp_user_id = 2;
     <meta charset="utf-8">
     <title>Géomarcheur</title>
 
+    <!-- TODO: Include all these from a separate file to prevent missing links -->
+
     <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
 
     <!--Import Google Icon Font-->
@@ -16,7 +18,7 @@ $temp_user_id = 2;
     <link type="text/css" rel="stylesheet" href="<?php echo base_url(); ?>static/css/materialize.min.css"  media="screen,projection"/>
     <link type="text/css" rel="stylesheet" href="<?php echo base_url(); ?>static/css/animate.css"  media="screen,projection"/>
     <!--Import custom css files-->
-    <link type="text/css" rel="stylesheet" href="<?php echo base_url(); ?>static/css/input_color_override.css"  media="screen,projection"/>
+    <link type="text/css" rel="stylesheet" href="<?php echo base_url(); ?>static/css/input_color_login_override.css"  media="screen,projection"/>
     <link type="text/css" rel="stylesheet" href="<?php echo base_url(); ?>static/css/style.css"  media="screen,projection"/>
 
     <!--Let browser know website is optimized for mobile-->
@@ -42,7 +44,30 @@ $temp_user_id = 2;
 <body>
 
 <main class="valign-wrapper pink darken-3">
-    <h1 class="white-text center-align full">Géomarcheur</h1>
+    <div class="container">
+        <h1 class="white-text center-align full">Géomarcheur</h1>
+        <div class="row animated fadeInUp">
+            <form class="col s12 m8 l6 offset-m2 offset-l3">
+                <div class="row">
+                    <div class="input-field col s12 white-text">
+                        <i class="material-icons prefix">account_circle</i>
+                        <input id="pseudo" type="text" class="validate" autofocus>
+                        <label for="pseudo">Pseudo</label>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="input-field col s12  white-text">
+                        <i class="material-icons prefix">lock</i>
+                        <input id="password" type="password" class="validate">
+                        <label for="password">Password</label>
+                    </div>
+                </div>
+            </form>
+            <p class="col s12 m8 l6 offset-m2 offset-l3"><a href="#" class="btn right indigo darken-4 waves-effect waves-light ">Connection</a></p>
+            <p class="col s12 m8 l6 offset-m2 offset-l3"><a href="#" class="white-text underline">S'inscrire</a></p>
+        </div>
+    </div>
+
 </main>
 
 <!--Import jQuery before materialize.js-->
